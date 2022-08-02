dde6d52304ebaf646dc1e07b0e55b1da737827cc
Filipstrozik <filipshelby@gmail.com>
2022-08-02 19:31:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const socket = io('http://localhost:3000')
+
+const messageForm = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
+const messageInput = document.getElementById('message-input')
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
+const SELECTIONS = [
+    {
+      name: 'rock',
+      emoji: '✊',
+      beats: 'scissors'
+    },
+    {
+      name: 'paper',
+      emoji: '✋',
+      beats: 'rock'
+    },
+    {
+      name: 'scissors',
+      emoji: '✌',
+      beats: 'paper'
+    }
+  ]
+
+const name = prompt('Jak masz na imię?')
+appendMessage(`${name} Dołączyłeś!`);
+socket.emit('new-user', name)
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection){
+    appendMessage(`Wybrales ${selection.emoji}`)
+    socket.emit('send-selection', selection)
+}
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} joined`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+socket.on('clear-chat', () => {
+    const divs = document.querySelectorAll('.msg')
+    console.log('cleared chat')
+    divs.forEach(e => {
+        e.remove()
+    });
+    
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`Ty: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.className = 'msg'
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
---
+++ server.js
@@ -0,0 +1,77 @@
+const io = require('socket.io')(3000)
+
+const users = {}
+var decisions = [] // lista obiektow typu seleckja 
+var noOfUsers = 0
+
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        noOfUsers+=1
+        socket.broadcast.emit('user-connected', name)
+    })
+    // console.log('new user')
+    // socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', {message: message, name:users[socket.id]})
+    })
+    socket.on('disconnect',() => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+        noOfUsers-=1
+    })
+    socket.on('send-selection', selection =>{
+        // decisions[socket.id] = selection
+        var newDecision = {
+            id: socket.id,
+            selection: selection,
+            win: false
+        }
+        decisions.push(newDecision);
+        if(noOfUsers === decisions.length){
+            console.log('Sprawdzam!')
+            for(var i = 0; i < decisions.length; i++){
+                for(var j = i+1; j < decisions.length; j++){
+                    var one = isWinner(decisions.at(i).selection, decisions.at(j).selection)
+                    if(one){
+                        decisions.at(i).win = true;
+                        // console.log(decisions.at(i))
+                    }
+                    var sec = isWinner(decisions.at(j).selection, decisions.at(i).selection)
+                    if(sec){
+                        decisions.at(j).win = true;
+                        // console.log(decisions.at(j))
+                    }
+                    // console.log(one)
+                    // console.log(sec)
+                }
+            }
+            for(var i = 0; i<decisions.length; i++){
+                if(decisions.at(i).win){
+                    io.emit('chat-message', {message: `${decisions.at(i).selection.emoji} Wygrał/a`, name:users[decisions.at(i).id]})//do
+                    console.log(decisions.at(i)) 
+                } else {
+                    io.emit('chat-message', {message: `${decisions.at(i).selection.emoji} Przegrał/a`, name:users[decisions.at(i).id]})//do
+                }
+
+            }
+            delay(5000).then(() => io.emit('clear-chat'));
+            decisions = []
+        } else {
+            socket.broadcast.emit('chat-message', {message: 'is ready!', name:users[socket.id]})
+        }
+        
+    })
+})
+
+
+
+
+function isWinner(selection, opponentSelection){
+    return selection.beats === opponentSelection.name;
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
