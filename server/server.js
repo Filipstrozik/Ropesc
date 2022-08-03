0821a1dd1a15c239254085ff368c7524ad7d6e2d
Filipstrozik <filipshelby@gmail.com>
2022-08-03 19:36:08 +0200

    smal tweaks

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')({
     cors: {
-        origin: "https://chic-eclair-d591d6.netlify.app/",
+        origin: "https://chic-eclair-d591d6.netlify.app",
         methods: ["GET", "POST"],
         credentials: true
     },
